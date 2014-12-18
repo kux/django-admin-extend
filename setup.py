c08460faaccf75acb43f8bab6e3248666ff811c6
Ioan Alexandru Cucu <alexandruioan.cucu@gmail.com>
2014-12-18 13:25:10 +0000

    Fix download url and bump version

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-admin-extend',
-    version='0.0.1',
+    version='0.0.2',
     description=('Provides functionality for extending'
                  'ModelAdmin classes that have already'
                  'been registered by other apps'),
     author='Ioan Alexandru Cucu',
     author_email='alexandruioan.cucu@gmail.com',
     url='https://github.com/kux/django-admin-extend',
-    download_url='https://github.com/kux/django-admin-extend/tarball/0.1',
+    download_url='https://github.com/kux/django-admin-extend/archive/0.0.2.tar.gz,
     install_requires=('Django>=1.3',),
     packages=find_packages(),
     include_package_data=True,
